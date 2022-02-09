2d9be88214119a67f3bd7c68df2947759435f954
Kristine Gusta <kristinegusta1@gmail.com>
2022-02-09 11:57:54 +0100

    adding media query and action log

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ const btn = document.querySelector(".btn")
 const listOfTd = document.querySelectorAll("td")
 const inputDisplay = document.querySelector("p")
 const result = document.querySelector(".result")
+const ul = document.querySelector("ul")
 
+//quicker way for this ?
 btn.addEventListener('click',(event) => {
 document.querySelector(".calculator").classList.toggle("calculator-light")
 document.querySelector(".btn").classList.toggle("btn-light")
@@ -13,9 +15,12 @@ document.querySelector("table").classList.toggle("table-light")
 listOfTd.forEach(element => {
     element.classList.toggle("td-light")
 });
+document.querySelector(".history").classList.toggle("history-light")
+document.querySelector("h1").classList.toggle("h1-light")
+document.querySelector("ul").classList.toggle("ul-light")
 })
 
-//Give event listeners to the TD that need to be displayed in the InputDisplay (aka everything except DELETE and Result)
+//Give event listeners to the TD 
 
 for (const td of listOfTd) {
     td.addEventListener('click',(event) => {
@@ -32,13 +37,27 @@ for (const td of listOfTd) {
               }
             let newResult = computeResult(inputDisplay.textContent)
             result.textContent = newResult
+            if(ul.childElementCount<5){
+                ul.insertAdjacentHTML('beforeend', `<li>${inputDisplay.textContent} = ${newResult}`)
+            }
+            else{
+                ul.removeChild((ul.getElementsByTagName('li')[0]))
+                ul.insertAdjacentHTML('beforeend', `<li>${inputDisplay.textContent} = ${newResult}`)
+            }
+            
         }
         else {
-            inputDisplay.textContent = inputDisplay.textContent + td.textContent
+            if(Number(result.textContent)>0){
+                //fix the input once there has been a calculation
+                inputDisplay.textContent = ""
+                result.textContent = ""
+            }
+                inputDisplay.textContent = inputDisplay.textContent + td.textContent
+            
         }
     })
 }
-
+// The same event listeners to keyboard buttons
 document.body.addEventListener('keyup',(event) => {
     if(event.code==="Enter"){
         function computeResult(str){
@@ -46,6 +65,7 @@ document.body.addEventListener('keyup',(event) => {
           }
         let newResult = computeResult(inputDisplay.textContent)
         result.textContent = newResult
+        ul.insertAdjacentHTML('beforeend', `<li>${inputDisplay.textContent} = ${newResult}`)
     }
     else if(event.code==="Backspace"){
         inputDisplay.textContent = inputDisplay.textContent.substring(0, inputDisplay.textContent.length - 1);
@@ -54,3 +74,5 @@ document.body.addEventListener('keyup',(event) => {
         inputDisplay.textContent = inputDisplay.textContent + event.key
     }
 })
+
+//Push computed results as li items in ul
